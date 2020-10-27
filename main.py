28628ef3028a48176c99b12bcdb12403331a2069
constant5 <crmarksv@gmail.com>
2020-10-27 14:34:19 -0500

    modified for new scraper

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from Reddit_Scraper.RedditScraper import redditscraper
 from LDA.LDA_Infer import lda_infer
 import numpy as np
 import pickle
+from GUI import gui_interface
 
+gui = gui_interface()
 
-RS = redditscraper()
+RS = gui.scraper
 LDA = lda_infer('LDA\\models\\hash_vect.pk', 'LDA\\models\\lda_model_8.pk')
 
-sports  = RS.Get_Reddit_Comments('sports', 20)
+sports  = RS.Get_Reddit_Comments('sports', 2)
 
-politics  = RS.Get_Reddit_Comments('politics', 20)
+politics  = RS.Get_Reddit_Comments('politics', 2)
 
 sports = [post for post in sports['title']]
 politics = [post for post in politics['title']]
