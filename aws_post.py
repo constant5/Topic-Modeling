df1415ffd5f11a4fd3d343c5fdb31d7b7c51c006
constant5 <crmarksv@gmail.com>
2020-10-27 07:43:37 -0500

    organized and cleaned up file structure

--- aws_post.py
+++ aws_post.py
@@ -1,17 +1,16 @@
 import requests
+
+
 credentials_file='aws_api_id.txt'
-CF = open(credentials_file, 'r')
-CF_Data = CF.read()
-CF.close()
+with open(credentials_file, 'r') as CF:
+    CF_Data = CF.read()
 
 credentials_dictionary = {}
 
 for Line in CF_Data.splitlines():
     if not Line.strip():
         continue
-
     Key, Value = Line.split('=')
-
     credentials_dictionary[Key.strip()] = Value.strip()
 
 credentials_dictionary
@@ -21,7 +20,7 @@ credentials_dictionary
 
 url = f"https://{credentials_dictionary['YOUR_API_KEY']}.execute-api.us-east-1.amazonaws.com/prod/get-topic-model"
 
-payload = 'how chow now little brown cow fo this test must be ata least ten chars long'
+payload = ' A Pelicans fan snuck on to the court for warmups, stretched and put up a shot before the police escorted him off'
 
 
 headers = {
